a4632df96dc75f40ff499563ebf4c2be597890d1
Kent Claudio <rikysmile33@gmail.com>
2021-01-16 21:17:24 -0800

    Updated COMP4537 Page

---
+++ public/scripts/comp4537.js
@@ -0,0 +1,7 @@
+// Live Server: http://127.0.0.1:5500/isa/public/index.html
+// Domain: https://kentc.herokuapp.com/
+
+function back_to_index() {
+    // window.location.href = 'http://127.0.0.1:5500/isa/public/index.html';
+    window.location.href = 'https://kentc.herokuapp.com/index.html';
+}
