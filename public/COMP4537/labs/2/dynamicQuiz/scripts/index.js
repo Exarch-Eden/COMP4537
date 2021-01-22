9f38d356a454b66a163b1eccec1397e29bc18155
Kent Claudio <rikysmile33@gmail.com>
2021-01-21 19:36:21 -0800

    Created directory for Lab 2 and respective files
    Created three html files with related css and js files. Also updated COMP4537.html to include link to Lab 2 Index Page

---
+++ public/COMP4537/labs/2/dynamicQuiz/scripts/index.js
@@ -0,0 +1,7 @@
+function toAdmin() {
+    window.location.href = "http://127.0.0.1:5500/public/COMP4537/labs/2/dynamicQuiz/admin.html";
+}
+
+function toStudent() {
+    window.location.href = "http://127.0.0.1:5500/public/COMP4537/labs/2/dynamicQuiz/student.html";
+}
