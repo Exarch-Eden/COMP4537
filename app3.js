4d200519d061f2c133482095ded4333b08ca29ce
Kent Claudio <rikysmile33@gmail.com>
2021-02-12 21:40:29 -0800

    Added GET request handler to partner 2 server and added button to lab 5 read and write html files

--- app3.js
+++ app3.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const express = require("express");
+const url = require("url");
 const date_server = require("./public/COMP4537/labs/4/getDate/server");
 
 const port = process.env.PORT || 8000;
@@ -10,6 +11,24 @@ app.use(express.static("public"));
 
 app.get("/COMP4537/labs/4/getDate/", function (req, res) {
   date_server.date_server(req, res);
+});
+
+app.get("https://billyvunguyen.com/COMP4537/labs/5/", function (req, res) {
+  let query = url.parse(req.url, true);
+  let queryData = query.query;
+  // let name = queryData.name;
+  // let score = queryData.score;
+
+  console.log(queryData);
+
+  res.statusCode = 200;
+
+  // if query is empty
+  if (Object.keys(q.query).length === 0) {
+    
+    res.end("name: " + name + " / score: " + score.toString());
+  }
+
 });
 
 http.createServer(app).listen(port);
