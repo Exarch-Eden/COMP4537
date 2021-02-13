f1e3cdfe5ad0b9d99f58acd51d3afa8e951d8b23
Kent Claudio <rikysmile33@gmail.com>
2021-02-12 23:17:13 -0800

    help

--- app3.js
+++ app3.js
@@ -13,23 +13,23 @@ app.get("/COMP4537/labs/4/getDate/", function (req, res) {
   date_server.date_server(req, res);
 });
 
-// app.get("https://billyvunguyen.com/COMP4537/labs/5/", function (req, res) {
-//   let query = url.parse(req.url, true);
-//   let queryData = query.query;
-//   let name = queryData.name;
-//   let score = queryData.score;
+app.get("https://billyvunguyen.com/COMP4537/labs/5/", function (req, res) {
+  let query = url.parse(req.url, true);
+  let queryData = query.query;
+  let name = queryData.name;
+  let score = queryData.score;
 
-//   console.log(queryData);
-//   // res.statusCode = 200;
+  console.log(queryData);
+  // res.statusCode = 200;
 
-//   // // if query is empty
-//   // if (Object.keys(q.query).length === 0) {
-//   //   res.writeHead(200, {
-//   //     "Content-Type": "text/html",
-//   //     "Access-Control-Allow-Origin": "*",
-//   //   });
-//   //   res.end("end");
-//   // }
-// });
+  // // if query is empty
+  // if (Object.keys(q.query).length === 0) {
+  //   res.writeHead(200, {
+  //     "Content-Type": "text/html",
+  //     "Access-Control-Allow-Origin": "*",
+  //   });
+  //   res.end("end");
+  // }
+});
 
 http.createServer(app).listen(port);
