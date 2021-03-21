cad751b2de8148aa3a3729a5655d2f44f4dd196c
Kent Claudio <rikysmile33@gmail.com>
2021-03-20 22:48:38 -0700

    help

--- app3.js
+++ app3.js
@@ -23,14 +23,14 @@ const noUpdatesFound = "No updates found";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, Content-Length, X-Requested-With"
-//   );
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+});
 
 // LOCAL DATABASE:
 // const con = mysql.createConnection({
