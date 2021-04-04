96378f2ee4eac5471af1725010c2f125684f2c2a
Kent Claudio <rikysmile33@gmail.com>
2021-04-03 21:38:33 -0700

    Updated admin page to render uri status data into table

--- public/COMP4537/termproject/api/v1/scripts/admin.js
+++ public/COMP4537/termproject/api/v1/scripts/admin.js
@@ -16,6 +16,10 @@ const uriStatusContainerId = "uriStatusContainer";
 // DOM create element tag constants
 const CREATE_UL = "ul";
 const CREATE_LI = "li";
+const CREATE_TABLE = "table";
+const CREATE_TR = "tr"; // table row
+const CREATE_TH = "th"; // table header
+const CREATE_TD = "td"; // table data
 
 // http method constants
 const GET = "GET";
@@ -29,29 +33,96 @@ const STATUS_200 = 200;
 
 // the maximum possible number of URIs
 const MAX_URIS = 10;
+// number of columns in a URI status
+const NUM_COLUMNS_URI = 4;
 
 window.onload = () => {
   renderUri();
 };
 
 const renderUri = () => {
-  // container for the uri unordered list
+  // container for the uri table
   const uriContainer = document.getElementById(uriStatusContainerId);
 
-  // list to hold the data extracted from URI table in database
-  const uriList = document.createElement(CREATE_UL);
+  // table to hold the data extracted from URI table in database
+  const uriTable = document.createElement(CREATE_TABLE);
 
   // execute GET method to grab the URI statuses from database
   const uriData = getUri();
 
-  // put data into unordered list
-  for (let i = 0; i < MAX_URIS; i++) {
-    const currentUri = document.createElement(CREATE_LI);
-    currentUri.innerText = `item ${i + 1}`;
-    uriList.appendChild(currentUri);
+  const rowHeaders = document.createElement(CREATE_TR);
+
+  // add the column headers of the table
+  for (let i = 0; i < NUM_COLUMNS_URI; i++) {
+    const curHeader = document.createElement(CREATE_TH);
+    curHeader.innerText = getHeader(i);
+    rowHeaders.appendChild(curHeader);
   }
 
-  uriContainer.appendChild(uriList);
+  // append row of column headers to table
+  uriTable.appendChild(rowHeaders);
+
+  // put data into uri table
+  uriData.map((curUri, index) => {
+    const curRow = document.createElement(CREATE_TR);
+
+    console.log(`curUri (index ${index}): \n${curUri}`);
+
+    for (let j = 0; j < NUM_COLUMNS_URI; j++) {
+      const curCol = document.createElement(CREATE_TD);
+
+      switch (j) {
+        case 0:
+          curCol.innerText = curUri.stat_id;
+          break;
+        case 1:
+          curCol.innerText = curUri.method;
+          break;
+        case 2:
+          curCol.innerText = curUri.endpoint;
+          break;
+        case 3:
+          curCol.innerText = curUri.requests;
+          break;
+      }
+
+      curRow.appendChild(curCol);
+    }
+
+    // currentUri.innerText = `item ${i + 1}`;
+
+    uriTable.appendChild(curRow);
+  });
+
+  // put data into uri table
+  // for (let i = 1; i < uriData.length; i++) {}
+
+  uriContainer.appendChild(uriTable);
+};
+
+const getHeader = (colNum) => {
+  if (typeof colNum !== number)
+    throw new Error("Parameter colNum is not of type number");
+  if (colNum > 3 || colNum < 0)
+    throw new Error("Parameter colNum value must be between 0-3");
+
+  let headerName = "";
+  switch (colNum) {
+    case 0:
+      headerName = "stat_id";
+      break;
+    case 1:
+      headerName = "method";
+      break;
+    case 2:
+      headerName = "endpoint";
+      break;
+    case 3:
+      headerName = "requests";
+      break;
+  }
+
+  return headerName;
 };
 
 const getUri = async () => {
@@ -61,60 +132,10 @@ const getUri = async () => {
     crossOriginPrefix + baseAPILink + apiUriStatus + `?api_key=${apiKey}`;
   console.log(`link: \n${url}`);
 
-  // const response = new Promise((resolve) => {
-  //   const xhttp = new XMLHttpRequest();
-
-  //   xhttp.open(GET, url, true);
-
-  //   xhttp.setRequestHeader("Content-Type", "application/json");
-
-  //   // (async () => {
-  //   // xhttp.send();
-
-  //   xhttp.onload = () => {
-  //     const status = xhttp.status;
-  //     console.log(`status: ${status}`);
-  //     if (status === STATUS_200) {
-  //       resolve(xhttp.responseText);
-  //     } else {
-  //       console.log("An error occured while attempting to read data");
-  //       reject(status);
-  //     }
-  //   };
-
-  //   // xhttp.onerror = () => {};
-
-  //   xhttp.send();
-
-  //   // xhttp.onreadystatechange = () => {
-  //   //   // try {
-  //   //   //   // while not ready and status is not OK (200)
-  //   //   //   while (!(xhttp.readyState === 4 && xhttp.status === STATUS_200)) {
-  //   //   //     console.log("not finished reading data");
-  //   //   //   }
-
-  //   //   //   console.log("await");
-
-  //   //   if (xhttp.readyState === 4 && xhttp.status === 200) {
-  //   //     console.log("finished reading data");
-
-  //   //     console.log(`responseText: \n${xhttp.responseText}`);
-  //   //   } else {
-  //   //     console.log("not finished reading data");
-  //   //   }
-  //   //   // } catch (err) {
-  //   //   //   console.log("An error occured while attempting to read data");
-  //   //   //   console.log(err);
-  //   //   // }
-  //   // };
-  // });
-
   console.log("awaiting uriData");
   uriData = await makeRequest(GET, url);
 
   console.log(`uriData: \n${uriData}`);
-
-  // })();
 
   console.log("end of getUri()");
 
@@ -129,9 +150,6 @@ const makeRequest = (method, url) => {
 
     xhttp.setRequestHeader("Content-Type", "application/json");
 
-    // (async () => {
-    // xhttp.send();
-
     xhttp.onload = () => {
       const status = xhttp.status;
       console.log(`status: ${status}`);
@@ -143,10 +161,9 @@ const makeRequest = (method, url) => {
       }
     };
 
-    // xhttp.onerror = () => {};
-
     xhttp.send();
 
+    // OLD CODE --------------------------------------------
     // xhttp.onreadystatechange = () => {
     //   // try {
     //   //   // while not ready and status is not OK (200)
@@ -169,4 +186,4 @@ const makeRequest = (method, url) => {
     //   // }
     // };
   });
-}
+};
